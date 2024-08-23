0ba7f5a473c62ac3a004fc8d64349812d8ca5806
ERICK Carvalho Paulette de Oliveira <erickzcarvalho25@outlook.com>
2024-08-23 12:46:41 -0300

    unit test icones-component

--- src/app/views/components/icones/icones.component.spec.ts
+++ src/app/views/components/icones/icones.component.spec.ts
@@ -1,23 +1,44 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Location } from '@angular/common';
 
 import { IconesComponent } from './icones.component';
+import { Router, RouterModule } from '@angular/router';
+import { HomeComponent } from '../../home/home.component';
+import { routes } from '../../../app.routes';
 
 describe('IconesComponent', () => {
   let component: IconesComponent;
   let fixture: ComponentFixture<IconesComponent>;
+  let router: Router;
+  let location: Location;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [IconesComponent]
-    })
-    .compileComponents();
-    
+      imports: [
+        RouterModule.forRoot(routes),
+        IconesComponent,
+        HomeComponent
+      ],
+    }).compileComponents();
+
+    router = TestBed.inject(Router);
+    location = TestBed.inject(Location);
     fixture = TestBed.createComponent(IconesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("deve renderizar o componente apenas na rota '/componentes/icones'", async () => {
+    await router.navigate(['componentes/icones']);
+    fixture.detectChanges();
+    expect(location.path()).toBe('/componentes/icones');
     expect(component).toBeTruthy();
+  });
+
+  it('não deve renderizar o componente em outras rotas', async () => {
+    await router.navigate(['inicio']);
+    fixture.detectChanges();
+    expect(location.path()).not.toBe('/componentes/icones');
+    expect(component).toBeTruthy();   // ou verifique se o elemento do componente está presente
   });
 });
