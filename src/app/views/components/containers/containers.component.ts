f72eaa34c2a98a1fc7d258c291397e277fa2fbaf
ERICK Carvalho Paulette de Oliveira <erickzcarvalho25@outlook.com>
2024-09-17 12:09:46 -0300

    publicação periodica

--- src/app/views/components/containers/containers.component.ts
+++ src/app/views/components/containers/containers.component.ts
@@ -1,12 +1,107 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { InfraModule } from 'ngx-sp-infra';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+
+import { InstallationInstructionsComponent } from '../../../shared/components/installation-instructions/installation-instructions.component';
+import { NavbarComponent } from '../../../shared/components/navbar/navbar.component';
+import { CodeSnippetComponent } from '../../../shared/components/code-snippet/code-snippet.component';
+import { UtilsService } from '../../../shared/services/utils.service';
 
 @Component({
   selector: 'app-containers',
   standalone: true,
-  imports: [],
+  imports: [
+    NavbarComponent,
+    CodeSnippetComponent,
+    CommonModule,
+    InfraModule,
+    ReactiveFormsModule,
+    RouterModule,
+    TooltipModule,
+    InstallationInstructionsComponent
+  ],
   templateUrl: './containers.component.html',
   styleUrl: './containers.component.scss'
 })
-export class ContainersComponent {
+export class ContainersComponent implements OnInit {
+
+  // #region ==========> PROPERTIES <==========
+
+  // #region PRIVATE
+  private _isCopied: boolean = false;
+  // #endregion PRIVATE
+
+  // #region PUBLIC
+
+
+  public codeSnippets: string[] = [
+    ``,
+    ``,
+    ``,
+    ``,
+    ``,
+    ``,
+  ];
+
+  public get isCopied(): boolean { return this._isCopied; }
+  public set isCopied(value: boolean) {
+    this._isCopied = value;
+    if (value) { setTimeout(() => { this._isCopied = false }, 3000) }
+  }
+  // #endregion PUBLIC
+
+  // #endregion ==========> PROPERTIES <==========
+
+
+  // #region ==========> FORM BUILDER <==========
+
+  // #region FORM FIELDS
+  // [...]
+  // #endregion FORM FIELDS
+
+  // #region FORM BUILDER
+  // [...]
+  // #endregion FORM BUILDER
+
+  // #endregion ==========> FORM BUILDER <==========
+
+
+  // #region ==========> INITIALIZATION <==========
+  constructor(
+    public utilsService: UtilsService
+  ) { }
+
+  ngOnInit() { }
+  // #endregion ==========> INITIALIZATION <==========
+
+
+  // #region ==========> SERVICE METHODS <==========
+
+  // #region PREPARATION
+  // [...]
+  // #endregion PREPARATION
+
+  // #region GET
+  // [...]
+  // #endregion GET
+
+  // #region POST
+  // [...]
+  // #endregion POST
+
+  // #region DELETE
+  // [...]
+  // #endregion DELETE
+
+  // #endregion ==========> SERVICE METHODS <==========
+
+
+  // #region ==========> UTILS <==========
+  // [...]
+  // #endregion ==========> UTILS <==========
 
 }
